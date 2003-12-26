09c2d17f94757f6d9547fa885c0036728789c513
crazyed <>
2003-12-26 19:14:49 +0000

    * Fixed $shuffle*() to respect extractw. * Altered $rest() so that a negative first argument makes it a compliment to   $right() in the same way that a positive arg makes it a compliment to   $left().  This may conceivably cause problems for scripts. * Spruced $joinstr() up a bit.  It now accepts any number of variables and   respects extractw. * Fixed an extractw bug in malloc_strcat_word_c while testing the above.
    * Added $splicec() in functions script.
    * Added /set dcc_dequote_filenames.  Perhaps this should default to off?

--- source/ircaux.c
+++ source/ircaux.c
@@ -1,4 +1,4 @@
-/* $EPIC: ircaux.c,v 1.103 2003/12/06 02:03:11 jnelson Exp $ */
+/* $EPIC: ircaux.c,v 1.104 2003/12/13 17:25:58 jnelson Exp $ */
 /*
  * ircaux.c: some extra routines... not specific to irc... that I needed 
  *
@@ -4102,7 +4102,7 @@ char *	malloc_strcat_wordlist_c (char **ptr, const char *word_delim, const char 
 char *	malloc_strcat_word_c (char **ptr, const char *word_delim, const char *word, size_t *clue)
 {
 	/* You MUST turn on extractw to get double quoted words */
-	if (!x_debug & DEBUG_EXTRACTW)
+	if (!(x_debug & DEBUG_EXTRACTW))
 		return malloc_strcat_wordlist_c(ptr, word_delim, word, clue);
 
 	if (word && *word)
