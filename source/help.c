c8ee5205369e25912ca405d1bb06adcda126b843
jnelson <>
2004-07-24 00:02:31 +0000

    Flush all of the small bug fixes i've been sitting on.

--- source/help.c
+++ source/help.c
@@ -1,4 +1,4 @@
-/* $EPIC: help.c,v 1.11 2003/10/10 06:09:01 jnelson Exp $ */
+/* $EPIC: help.c,v 1.12 2004/02/20 23:40:23 jnelson Exp $ */
 /*
  * help.c: handles the help stuff for irc 
  *
@@ -735,6 +735,8 @@ static	void create_help_window (void)
 		help_window->holding_top_of_display = NULL;
 		recalculate_window_cursor_and_display_ip(help_window);
 		help_window->window_level = LOG_HELP;
+		window_body_needs_redraw(help_window);
+		window_statusbar_needs_redraw(help_window);
 		update_all_windows();
 	}
 	else
