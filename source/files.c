7d3a08fc552a50d54377c8408849e559380100c4
jnelson <>
2008-01-15 13:04:54 +0000

    * New version of /load hold_mode (implements /set hold_interval) * Fix crash with $write(w<number> text)

--- source/files.c
+++ source/files.c
@@ -1,4 +1,4 @@
-/* $EPIC: files.c,v 1.21 2003/10/31 08:19:24 crazyed Exp $ */
+/* $EPIC: files.c,v 1.22 2003/11/07 03:55:57 jnelson Exp $ */
 /*
  * files.c -- allows you to read/write files. Wow.
  *
@@ -187,8 +187,10 @@ static File *lookup_logfile (int fd)
 
 	if (fd == -1)
 		x = irclog_fp;
-	if ((w = get_window_by_refnum(fd)))
+	else if ((w = get_window_by_refnum(fd)))
 		x = w->log_fp;
+	else
+		return NULL;
 
 	retval.file = x;		/* XXX Should be a file */
 	retval.next = NULL;
